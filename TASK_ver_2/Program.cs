3328586a5a0bbe899c28ac1e658755b9d939d715
Sergey <sergbond61@gmail.com>
2022-12-23 14:27:33 +0300

    добавил код альтернативного решения

---
+++ TASK_ver_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите количество элементов исходного массива: ");
+int size = Convert.ToInt32(ReadLine());
+string[] finArr = new string[size];
+int len = 3;
+int pos = 0;
+for (int i = 0; i < size; i++)
+{
+    WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(ReadLine());
+    if (element.Length <= len)
+    {
+        finArr[pos] = element;
+        pos++;
+    }
+}
+WriteLine();
+WriteLine("Производный(искомый) массив:");
+PrintArray(finArr);
+void PrintArray(string[] array)
+{
+    Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        Write(array[i] + " ");
+    }
+    Write("]");
+}
